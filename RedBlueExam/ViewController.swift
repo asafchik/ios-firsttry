f519e665326ad6bc5b18dafe94c325706aa52631
Asaf Abramov <asafabramov@Adis-Mac.local>
2015-09-15 22:34:24 +0300

    added first commit

--- RedBlueExam/ViewController.swift
+++ RedBlueExam/ViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var blueBomb: UIImageView!
+    @IBOutlet weak var redBomb: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +24,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func blueButton(sender: AnyObject) {
+        blueBomb.hidden = true
+    }
 
+    @IBAction func redButton(sender: AnyObject) {
+        redBomb.hidden = true
+    }
 }
 
